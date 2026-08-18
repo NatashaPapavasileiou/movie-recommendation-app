584257045223f0e83cd448f45673b1655c17e18a
natasha <napaue@gmail.com>
2026-08-18 21:23:37 +0300

    Update API links to include region parameter
    Added region parameter to upcoming and now_playing endpoints.

--- src/modules/ApiLinks.ts
+++ src/modules/ApiLinks.ts
@@ -4,8 +4,8 @@ export const baseUrl = "https://api.themoviedb.org/3";
 
 //Movies Endpoints to fetch information 
 export const popular = `${baseUrl}/movie/popular?api_key=${apiKey}`
-export const upcoming = `${baseUrl}/movie/upcoming?api_key=${apiKey}`;
-export const now_playing = `${baseUrl}/movie/now_playing?api_key=${apiKey}`;
+export const upcoming = `${baseUrl}/movie/upcoming?api_key=${apiKey}&region=GR`;
+export const now_playing = `${baseUrl}/movie/now_playing?api_key=${apiKey}&region=GR`;
 export const top_rated_movies = `${baseUrl}/movie/top_rated?api_key=${apiKey}`;
 
 
